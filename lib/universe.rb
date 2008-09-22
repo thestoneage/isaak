aab319875c15bcad45d8ec183218621b1a94603b
Marc Rummel <marc.rummel@gmail.com>
2008-09-22 21:48:40 +0200

    A class that glues things together.

---
+++ lib/universe.rb
@@ -0,0 +1,42 @@
+class Universe
+
+  attr_reader :width, :height, :things
+
+  def initialize(width, height, gravity)
+    @things = []
+    @width = width
+    @height = height
+    @border = true
+    @gravity = gravity
+  end
+
+  def update
+    @things.each do |thing|
+      if @border
+        bounce(thing)
+      end
+      thing.add_force(@gravity * thing.mass)
+      thing.update
+    end
+  end
+  
+  def bounce(thing)
+    if thing.loc.x < 0 + thing.radius
+      thing.loc.x = 0 + thing.radius
+      thing.vel.x *= -1 * thing.elastic
+    elsif thing.loc.x > @width - thing.radius
+      thing.loc.x = @width - thing.radius
+      thing.vel.x *= -1 * thing.elastic
+    end
+    if thing.loc.y < 0 + thing.radius
+      thing.loc.y = 0 + thing.radius
+      thing.vel.y *= -1 * thing.elastic
+    elsif thing.loc.y > @height - thing.radius
+      thing.loc.y = @height - thing.radius
+      thing.vel.y *= -1 * thing.elastic
+    end
+    
+  end
+
+
+end
